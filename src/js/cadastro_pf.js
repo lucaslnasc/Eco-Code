dd7e99eed88ecbe4657d6724b9b6c10b155056c2
lucaslnasc <lucaslnascimento015@gmail.com>
2024-04-20 18:07:24 -0300

    init loginConnect user

---
+++ src/js/cadastro_pf.js
@@ -0,0 +1,34 @@
+// função para redirecionamento
+function BackIn(){
+    location.href = '../views/cadastre-se.html'
+}
+
+function CadastroPF() {
+    location.href = '../views/cadastro_pf.html'
+}
+
+// função para fazer validações
+
+function validate() {
+    var nome            =            document.getElementById('nome')
+    var email           =           document.getElementById('email')
+    var cep             =             document.getElementById('cep')
+    var bairro          =          document.getElementById('bairro')
+    var numero          =          document.getElementById('n')
+    var senha           =           document.getElementById('Senha')
+    var cpf             =             document.getElementById('cpf')
+    var ddd             =             document.getElementById('ddd')
+    var telefone        =        document.getElementById('telefone')
+    var endereco        =        document.getElementById('endereco')
+    var cidade          =          document.getElementById('cidade')
+    var confirmar_senha = document.getElementById('confirmar_senha')
+
+    var validate_null_input = nome != null && email != null && cep != null && bairro != null && numero != null && senha != null && cpf != null && ddd != null && telefone != null && endereco != null && cidade != null && confirmar_senha != null
+
+    if(validate_null_input) {
+        var input = document.getElementsByTagName('input')
+        input.style.border = 'green'
+    }else{
+        return 'preencha os campos corretamente'
+    }
+}
---
+++ src/models/user.php
@@ -0,0 +1,16 @@
+<?php
+include('../eco-code/database/conexao.php');
+
+function checkIfExists($column, $value){
+    global $dbh;
+
+    // Prepara a consulta SQL para contar quantas vezes o valor aparece na coluna especificada
+    $query = $dbh->prepare("SELECT COUNT(*) as count FROM usuario WHERE $column = :value");
+    $query->bindParam(':value', $value); // Substitui o marcador de posição na consulta pelo valor fornecido
+    $query->execute(); // Executa a consulta
+    $result = $query->fetch(PDO::FETCH_ASSOC); // Obtém o resultado da consulta como um array associativo
+
+    // Retorna true se o valor já existe na coluna, caso contrário, retorna false
+    return $result['count'] > 0;
+}
+?>
